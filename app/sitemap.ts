7a4406730ba0660c6801675d3f914ffd827ebc66
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-06-26 12:38:55 +0200

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,11 @@
+export default async function sitemap() {
+  return [
+    {
+      url: 'https://ubuntuafrican.co.za/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    // Add more URLs as needed
+  ];
+}
