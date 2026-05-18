7729ffba3bfa7bb3070ed0ae025f62040c668c62
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2026-05-19 01:45:48 +0200

    feat: update sitemap domain and routes, and add eslint dependencies to package.json

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,19 @@
 export default async function sitemap() {
   return [
     {
-      url: 'https://ubuntuafrican.co.za/',
+      url: 'https://ubuntuafrican.qwabi.co.za/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://ubuntu african.qwabi.co.za/about-developer',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://ubuntuafrican.qwabi.co.za/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
